1e62d70adaa43ed8d6d2d98043c83440b64b9744
hkgumbs <h.kofigumbs@gmail.com>
2015-04-12 02:47:51 -0400

    move to socket.io

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
-var express = require("express")
-var app = express()
-var port = process.env.PORT || 5000
+var server = require('http').createServer();
+var io = require('socket.io')(server);
 
-app.use(express.static(__dirname + "/"));
+var buffer = null;
 
-var server = http.createServer(app);
-server.listen(port);
-console.log("http server listening on %d", port)
+io.on('connection', function(socket) {
+    if (buffer == null || buffer.disconnected) {
+        buffer = socket;
+        console.log("waiting");
+    } else {
 
-var wss = new WebSocketServer({server: server});
-console.log("websocket server created")
+        // each socket should listen on its own id
+        buffer.on(buffer.id, function(data){
+            socket.emit('message', data);
+        });
+        socket.on(socket.id, function(data){
+            buffer.emit('message', data);
+        });
 
-var Battler = require('./battler');
-var battler = new Battler();
+        // initiate handshake
+        buffer.emit('message', {
+            id : buffer.id,
+            phase : 'handshake',
+            first : true
+        });
+        socket.emit('message', {
+            id : socket.id,
+            phase : 'handshake',
+            first : false
+        });
 
-wss.on('connection', battler.connect);
-wss.on('message', battler.message);
-wss.on('closedconnection', battler.close);
+        console.log('connected');
+        buffer = null;
+    }
+
+    // Test app
+    // socket.emit('message', {id : socket.id, val : 0});
+    // socket.on(socket.id, function(data) {
+    //     console.log(data.val);
+    //     socket.emit('message', {val : data.val + 1});
+    // });
+});
+
+server.listen(3000);
